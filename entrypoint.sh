ea6d58b9a31292c83e001718e9f490068a3b29b3
Thuan Duong <mrtux.hdb@gmail.com>
2018-12-17 15:51:34 +0700

    Add DATA_OPTS for initial sync data

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,9 @@ JMX_OPTS="-Dcom.sun.management.jmxremote.port=${JMX_PORT} -Dcom.sun.management.j
 JAVA_OPTS_DOCKER="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -Xms64M"
 set +u
 JAVA_OPTS="$JAVA_OPTS $JAVA_OPTS_DOCKER"
+if [[ -n $DATA_SYNC ]]; then
+    DATA_OPTS="--data"
+fi
 set -u
 
 TABLE=${TABLE:-Test_Table}
@@ -16,13 +19,15 @@ NUM_CONNECTIONS=${NUM_CONNECTIONS:-16}
 SCAN_RATE=${SCAN_RATE:-25}
 SCHEMA_NAME=${SCHEMA_NAME:-public}
 
+# shellcheck disable=SC2086
 exec java $JMX_OPTS \
           $JAVA_OPTS \
           -jar /app/podyn.jar \
+          $DATA_OPTS \
           --changes \
           --table "${TABLE}" \
           --postgres-jdbc-url "${POSTGRES_JDBC_URL}" \
           --num-connections "${NUM_CONNECTIONS}" \
           --scan-rate "${SCAN_RATE}" \
           -sn "${SCHEMA_NAME}" \
-          com.citusdata.migration.DynamoDBReplicator $*
+          com.citusdata.migration.DynamoDBReplicator "$@"
